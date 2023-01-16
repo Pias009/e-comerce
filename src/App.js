93065f2ecba0271b28ba0b40098624e8ac997262
Pias009 <pvs178380@gmail.com>
2023-01-16 17:09:33 +0600

    xc

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Products from './Products';
+import Contact from './Contact';
+import Singleproduct from './Singleproduct';
+import Cart from './Cart';
+import Err from './Err';
+import Nave from './component/Nave';
+
+
+
 
 function App() {
-  return (
-  <></>
+  
+return (
+    <>
+    <Nave/>
+   
+ <Router>
+ 
+  <Routes>
+  
+    <Route path='/' element={<Home />} />
+    <Route path='/about' element={<About/>} />
+    <Route path='/products' element={<Products/>} />
+    <Route path='/contact' element={<Contact/>} />
+    <Route path='/cart' element={<Cart/>} />
+    <Route path='/singleproduct/:id' element={<Singleproduct/>} />
+    <Route path='/err' element={<Err/>} />
+  </Routes>
+ </Router>
+
+</>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Home.css'
+
+
+export default function Home() {
+  
+  
+  return (
+    <> 
+
+    <div id="Home">
+        <img id="cyber" src="./img/cy.jpg" alt=""  />
+        <div id='Name'>
+        hello i am maclaren
+        </div>
+        <a href="./Singleproduct" type="button" className="btn-Mc">More info</a>
+    </div>
+    <div id="Home-2">
+        <img id="x" src="./img/x.jpg" alt=""  />
+        <div id='Gx'>
+        hello i am maclaren
+        </div>
+        <a href="./Singleproduct" type="button" className="btn-Gx">More info</a>
+        
+        
+    </div>
+ 
+    
+    </>
+  )
+}
